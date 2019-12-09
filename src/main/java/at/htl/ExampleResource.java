07d9f012788e1aaaa031dfd8eb4ea9472acc22e3
Leon Schlömmer <leon.schloemmer@posteo.at>
2019-12-10 00:46:05 +0100

    got jwt authorization to work

--- src/main/java/at/htl/ExampleResource.java
+++ src/main/java/at/htl/ExampleResource.java
@@ -1,12 +1,14 @@
 package at.htl;
 
 import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+@RequestScoped
 public class ExampleResource {
 
     @GET
